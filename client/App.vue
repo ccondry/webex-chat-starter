4533958cf2f047eb084a81c5de7dd1f1d681392f
Coty Condry <ccondry@cisco.com>
2019-06-03 15:05:22 -0500

    add CJP-webex toolbox link. also move admin section to its own article

--- client/App.vue
+++ client/App.vue
@@ -26,22 +26,33 @@
               <li>
                 <a href="/cjp-ccone">CJP CCOne Demo</a>
               </li>
+              <li>
+                <a href="/cjp-webex">CJP Webex Demo</a>
+              </li>
             </ul>
-            <!-- Admin pages -->
-            <div v-if="user.admin">
-              <p>
-                Admin Pages
-              </p>
-              <ul>
-                <li>
-                  <a href="/rcdn">RCDN (Compete Lab) Demos (in development)</a>
-                </li>
-              </ul>
-            </div>
           </div>
         </article>
       </div>
     </div>
+
+    <!-- Admin pages -->
+    <div class="tile is-ancestor" v-if="user.admin">
+      <div class="tile is-parent is-3">
+      </div>
+      <div class="tile is-parent is-6">
+        <article class="tile is-child box">
+          <h1 class="title">Admin Pages</h1>
+          <div class="content">
+            <ul>
+              <li>
+                <a href="/rcdn">RCDN (Compete Lab) Demos (in development)</a>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </div>
+
   </div>
 </template>
 
