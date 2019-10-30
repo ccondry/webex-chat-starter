2c3adb14ae179a4be77e5b884434ee8a396d39f1
Coty Condry <ccondry@cisco.com>
2019-10-30 13:59:07 -0500

    put isAuthenticated requirement back in

--- client/App.vue
+++ client/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <b-loading :is-full-page="true" :active="!authCheckDone" :can-cancel="false"></b-loading>
-    <div>
+    <div v-if="isAuthenticated">
+
       <navbar :show="true"></navbar>
       <section class="app-main">
         <div class="container is-fluid is-marginless app-content">
