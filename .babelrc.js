774d4051d3298e297f5d8fb23072faa66e4b91b2
Coty Condry <ccondry@cisco.com>
2021-02-18 12:43:16 -0600

    update babel and webpack-bundle-analyzer

--- .babelrc.js
+++ .babelrc.js
@@ -1,48 +1,48 @@
-const commonjs = process.env.BABEL_ENV !== 'esm';
+const commonjs = process.env.BABEL_ENV !== "esm";
 
 module.exports = {
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
         loose: true,
         shippedProposals: true,
-        // modules: commonjs ? 'commonjs' : false,
-        modules: 'amd',
+        modules: commonjs ? "commonjs" : false,
         targets: {
           browsers: [
-            '>0.25%',
-            'ie >= 11',
-            'not op_mini all' // opera mini doesn't support client side
-          ]
-        }
-      }
+            ">0.25%",
+            "ie >= 11",
+            "not op_mini all", // opera mini doesn't support client side
+          ],
+        },
+      },
     ],
     [
-      '@vue/babel-preset-jsx',
+      "@vue/babel-preset-jsx",
       {
-        'vModel': false
-      }
-    ]
+        vModel: false,
+      },
+    ],
   ],
-  "env": {
-    "test": {
-      "presets": [
-        ["env", {  "targets": { "node": "current" }}]
-      ]
-    }
+  env: {
+    test: {
+      presets: [["env", { targets: { node: "current" } }]],
+    },
   },
   plugins: [
-    'transform-vue-jsx',
+    "@vue/babel-plugin-transform-vue-jsx",
     [
       "module-resolver",
       {
-      }
+        alias: {
+          "@momentum-ui/vue": "./src/lib",
+        },
+      },
     ],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    ['@babel/plugin-transform-runtime', { useESModules: true }],
-    'babel-plugin-dev-expression'
-  ].filter(Boolean)
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-proposal-export-namespace-from",
+    ["@babel/plugin-transform-runtime", { useESModules: !commonjs }],
+    "babel-plugin-dev-expression",
+  ].filter(Boolean),
 };
