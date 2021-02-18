6c84837fbc889492fc35673828cc915c31c8d84c
Coty Condry <ccondry@cisco.com>
2021-02-18 17:36:32 -0600

    remove Join Support Room button

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -4,52 +4,17 @@
       Welcome to the dCloud
       <strong>Cloud Configuration</strong>
       Toolbox!
-      <br>
-      Join our Webex Teams support room to get help, ask questions, and
-      suggest new features:
     </p>
-    <b-field>
-      <b-button
-      type="is-primary"
-      rounded
-      expanded
-      @click="clickJoinSupportRoom"
-      >
-        Join Support Room
-      </b-button>
-    </b-field>
   </panel>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters([
       'jwtUser'
     ])
-  },
-
-  methods: {
-    ...mapActions([
-      'joinSupportRoom'
-    ]),
-    clickJoinSupportRoom () {
-      this.$buefy.dialog.prompt({
-        title: 'Join Webex Teams Support Room',
-        message: 'What email address do you use for Webex Teams?',
-        rounded: true,
-        confirmText: 'Submit',
-        type: 'is-success',
-        inputAttrs: {
-          value: this.jwtUser.email,
-          placeholder: 'Your Webex Teams Email Address'
-        },
-        onConfirm: (email) => {
-          this.joinSupportRoom(email)
-        }
-      })
-    }
   }
 }
 </script>
