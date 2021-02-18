780023b8614ced4c6db575f3bca22440f9279eb1
Coty Condry <ccondry@cisco.com>
2021-02-18 14:09:41 -0600

    testing buefy loading components only

--- src/main.js
+++ src/main.js
@@ -11,8 +11,23 @@ import Buefy from 'buefy/src'
 // or use 'buefy' when running from npm
 // import Buefy from 'buefy'
 
+// import specific buefy components used in this project
+// import {
+//   ConfigProgrammatic,
+//   Table,
+//   Input
+// } from 'buefy'
+
 // add Buefy to Vue
 Vue.use(Buefy)
+
+// add specific Buefy components to Vue
+// Vue.use(Table)
+// Vue.use(Input)
+// ConfigProgrammatic.setOptions({
+// defaultIconPack: 'fas',
+// defaultContainerElement: '#content',
+// })
 
 // load panel component globally
 Vue.component('Panel', panel)
